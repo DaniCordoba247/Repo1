022029925f36ff0a59317ce2ef1b48439b6eb28d
horabaixa <horabaixa@daw1-34>
2024-02-21 18:38:59 +0100

    version 1

---
+++ Prac1/src/Act1/Main.java
@@ -0,0 +1,14 @@
+package Act1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Medio me1 = new Medio();
+		Mp3 mp1 = new Mp3();
+		Ogg o1 = new Ogg();
+		
+		
+	}
+
+}
---
+++ Prac1/src/Act1/Medio.java
@@ -0,0 +1,10 @@
+package Act1;
+
+public abstract class Medio {
+
+	private String nombre;
+	private double duracion;
+	
+	public abstract void reproducir();
+}
+
